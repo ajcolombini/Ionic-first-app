fa6dd1ce20cc6616d0fc660f3b73b4a713804fb9
ajcolombini <ajcolombini@gmail.com>
2017-08-06 00:40:25 -0300

    Ionic.App1

---
+++ src/pages/intro/intro.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from "../tabs/tabs";
+
+/**
+ * Generated class for the IntroPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-intro',
+  templateUrl: 'intro.html',
+})
+export class IntroPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad IntroPage');
+  }
+
+  slides = [
+    {
+      title: "Meu 1o. App com Ionic!",
+      description: "Esse <b>Ionic App</b> vai ficar muito legal!!!",
+      image: "../assets/images/slidebox-img-1.png",
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "../assets/images/slidebox-img-2.png",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "../assets/images/slidebox-img-3.png",
+    }
+  ];
+
+  goToTabsPage(){
+    this.navCtrl.push(TabsPage)
+  }
+
+
+}
